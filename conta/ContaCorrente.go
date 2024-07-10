6fc4996ae85e090b5920cf72adcaa5332dd00a71
Led-New <wendellmottasilva356@gmail.com>
2024-07-10 13:49:58 -0300

    segundoCommit

--- conta/ContaCorrente.go
+++ conta/ContaCorrente.go
@@ -1,38 +1,44 @@
 package contas
 
-import "BancoCentral/clientes"
+import (
+	"Banco/clientes"
+)
 
 type ContaCorrente struct {
-    Titular       clientes.Titular
-    NumeroAgencia  int
-    NumeroConta    int
-    Saldo          float64
+	Titular       clientes.Titular
+	NumeroAgencia int
+	NumeroConta   int
+	saldo         float64
 }
+
 func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-    podeSacar := valorDoSaque > 0 && valorDoSaque <= c.Saldo
-    if podeSacar {
-        c.Saldo -= valorDoSaque
-        return "Saque realizado com sucesso"
-    } else {
-        return "Saldo insuficiente"
-    }
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "saldo insuficiente"
+	}
 }
 
 func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
-    if valorDoDeposito > 0 {
-        c.Saldo += valorDoDeposito
-        return "Deposito realizado com sucesso", c.Saldo
-    } else {
-        return "Valor do deposito menor que zero", c.Saldo
-    }
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Deposito realizado com sucesso", c.saldo
+	} else {
+		return "Valor do deposito menor que zero", c.saldo
+	}
 }
 
 func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
-    if valorDaTransferencia < c.Saldo && valorDaTransferencia > 0 {
-        c.Saldo -= valorDaTransferencia
-        contaDestino.Depositar(valorDaTransferencia)
-        return true
-    } else {
-        return false
-    }
+	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
+		c.saldo -= valorDaTransferencia
+		contaDestino.Depositar(valorDaTransferencia)
+		return true
+	} else {
+		return false
+	}
+}
+func (c *ContaCorrente) SaldoConta() float64 {
+	return c.saldo
 }
